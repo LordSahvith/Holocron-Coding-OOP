24c0cf765f7a99e3b1d144c12cc24e4a3e1afcd1
LordSahvith <bulletproofchild@gmail.com>
2025-02-10 21:45:24 -0700

    first program

---
+++ matrix/courses/udemy/C++/Beginner-Beyond/01Program/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int FavoriteNumber;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+
+    std::cin >> FavoriteNumber;
+
+    std::cout << "No way! " << FavoriteNumber << " is my favorite too!" << std::endl; 
+
+    return 0;
+}
