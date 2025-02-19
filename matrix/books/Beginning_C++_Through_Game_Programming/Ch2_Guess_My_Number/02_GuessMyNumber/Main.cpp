7f165bba4e23a0157fba5dc6dca4c84961986abf
LordSahvith <bulletproofchild@gmail.com>
2025-02-18 22:23:41 -0700

    Add game loop and difficulty

--- matrix/books/Beginning_C++_Through_Game_Programming/Ch2_Guess_My_Number/02_GuessMyNumber/Main.cpp
+++ matrix/books/Beginning_C++_Through_Game_Programming/Ch2_Guess_My_Number/02_GuessMyNumber/Main.cpp
@@ -9,21 +9,58 @@ using namespace std;
 
 int main()
 {
+    cout << "\n\nWelcome to Guess my Number\n\n";
+
     bool playAgain = true;
+    bool guessesLeft = true;
 
     while (playAgain)
     {
+        cout << "Difficulty: " << endl;
+        cout << "1 - Easy" << endl;
+        cout << "2 - Medium" << endl;
+        cout << "3 - Hard" << endl;
+
+        int difficultyChosen;
+        cout << "\nChoose Difficulty: ";
+        cin >> difficultyChosen;
+
+        enum difficulty
+        {
+            EASY = 10,
+            MEDIUM = 6,
+            HARD = 3
+        };
+
+        difficulty maxTries;
+
+        switch (difficultyChosen)
+        {
+        case 1:
+            maxTries = EASY;
+            break;
+        case 2:
+            maxTries = MEDIUM;
+            break;
+        case 3:
+            maxTries = HARD;
+            break;
+        default:
+            maxTries = EASY;
+        }
+
+        cout << "\nMax Tries: " << maxTries << endl;
+
+
         // seed random number generator
         srand(static_cast<unsigned int>(time(0)));
         int secretNumber = rand() % 100 + 1; // 1-100
         int tries = 0;
         int guess;
 
-        cout << "\n\n\tWelcome to Guess my Number\n\n";
-
         do
         {
-            cout << "Enter your guess: ";
+            cout << "\nEnter your guess: ";
             cin >> guess;
             ++tries;
 
@@ -42,7 +79,12 @@ int main()
                 cout << "\nToo low!\n\n";
             }
 
-        } while (guess != secretNumber);
+            // if (tries == maxTries)
+            // {
+            //     guessesLeft = false;
+            // }
+
+        } while (guess != secretNumber && guessesLeft);
 
         char choice;
         cout << "\nNice job! Play again? (y/n): ";
