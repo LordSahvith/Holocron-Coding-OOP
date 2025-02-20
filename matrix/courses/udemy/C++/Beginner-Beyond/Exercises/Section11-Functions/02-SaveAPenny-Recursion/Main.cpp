d7cb141a1acb63ae9114eead484b7b20601bab26
LordSahvith <bulletproofchild@gmail.com>
2025-02-20 01:36:48 -0700

    Save a Penny Exercise : Recursion

---
+++ matrix/courses/udemy/C++/Beginner-Beyond/Exercises/Section11-Functions/02-SaveAPenny-Recursion/Main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+double a_penny_doubled_everyday(int days, double& amount);
+
+void PlayAgain(bool& again);
+
+int main()
+{
+    int days{0};
+
+    bool testAgain{true};
+
+    while (testAgain)
+    {
+        cout << "Enter Number of Days: ";
+        cin >> days;
+
+        double totalAmount{0.01};
+        a_penny_doubled_everyday(days, totalAmount);
+        cout << "\ntotal amount : $" << totalAmount << endl;
+
+        PlayAgain(testAgain);
+    }
+
+    return 0;
+}
+
+double a_penny_doubled_everyday(int days, double& amount)
+{
+    cout << "\nDay " << days << ": $" << amount;
+    
+    if (days <= 1)
+    {
+        return 0;
+    }
+
+    return a_penny_doubled_everyday(days - 1, amount *= 2.0);
+}
+
+void PlayAgain(bool& again)
+{
+    char choice;
+    cout << "Test Again? (y/n): ";
+    cin >> choice;
+    if (choice == 'n')
+    {
+        again = false;
+    }
+}
