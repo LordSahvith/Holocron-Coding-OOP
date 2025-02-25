d64fba72ff5ad7a98a6fe3f3e58faa9f97befda7
LordSahvith <bulletproofchild@gmail.com>
2025-02-24 21:49:53 -0700

    Member Access Modifiers : Member Functions

--- matrix/courses/udemy/C++/Beginner-Beyond/01_Examples/Sections/13-OOP/01-Basics/Main.cpp
+++ matrix/courses/udemy/C++/Beginner-Beyond/01_Examples/Sections/13-OOP/01-Basics/Main.cpp
@@ -8,62 +8,105 @@ using namespace std;
 
 class Player
 {
-  public:
+  private:
     // attributes
-    string name;
+    string Name;
     int health;
     int xp;
 
+  public:
     // methods
     void Talk(string textToSay)
     {
-        cout << name << " says " << textToSay << endl;
+        cout << Name << " says " << textToSay << endl;
     }
     bool IsDead();
 };
 
 class Account
 {
-  public:
+  private:
     // attributes
-    string name;
-    double balance;
+    string Name;
+    double Balance;
 
+  public:
     // methods
-    bool Deposit(double bal)
+    string GetName();
+    void SetName(string name);
+    void SetBalance(double amount);
+    bool Deposit(double amount);
+    bool Withdraw(double amount);
+};
+
+string Account::GetName()
+{
+    return Name;
+}
+
+void Account::SetName(string name)
+{
+    Name = name;
+}
+
+void Account::SetBalance(double amount)
+{
+    Balance = amount;
+}
+
+bool Account::Deposit(double amount)
+{
+    Balance += amount;
+    cout << "Deposit" << endl;
+    return true;
+}
+
+bool Account::Withdraw(double amount)
+{
+    if (Balance - amount >= 0)
     {
-        balance += bal;
-        cout << "Deposit" << endl;
-        return true;
-    }
-    bool Withdraw(double bal)
-    {
-        balance -= bal;
+        Balance -= amount;
         cout << "Withdraw" << endl;
         return true;
     }
-};
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
-    Player Savith;
-
-    Savith.name = "Savith";
-    Savith.health = 100;
-    Savith.xp = 4;
-
-    Player* Enemy = new Player;
-    (*Enemy).name = "Enemy";
-    (*Enemy).health = 100;
-    Enemy->xp = 16;
-    Enemy->Talk("I will destroy you!");
-
     Account SavithAccount;
-    SavithAccount.name = "Savith's Account";
-    SavithAccount.balance = 40000.00;
+    SavithAccount.SetName("Savith");
+    SavithAccount.SetBalance(1000.0);
 
-    SavithAccount.Deposit(1000.0);
-    SavithAccount.Withdraw(400.0);
+    if (SavithAccount.Deposit(200.0))
+    {
+        cout << "Deposit OK." << endl;
+    }
+    else
+    {
+        cout << "Deposit error." << endl;
+    }
+
+    if (SavithAccount.Withdraw(500.0))
+    {
+        cout << "Withdrawl OK." << endl;
+    }
+    else
+    {
+        cout << "Insuffient funds." << endl;
+    }
+
+    if (SavithAccount.Withdraw(1500.0))
+    {
+        cout << "Withdrawl OK." << endl;
+    }
+    else
+    {
+        cout << "Insuffient funds." << endl;
+    }
 
     return 0;
 }
