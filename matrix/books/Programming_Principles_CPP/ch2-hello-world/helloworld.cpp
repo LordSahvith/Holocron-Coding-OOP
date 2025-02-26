9bd558d80193714061d4723acaacb275c657a1ef
LordSahvith <bulletproofchild@gmail.com>
2025-02-25 22:03:29 -0700

    hello world

--- matrix/books/Programming_Principles_CPP/ch2-hello-world/helloworld.cpp
+++ matrix/books/Programming_Principles_CPP/ch2-hello-world/helloworld.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
-
-// using namespace std;
+#include "../00_std_lib_facilities.h"
 
 int main()
 {
     // cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
     return 0;
 }
