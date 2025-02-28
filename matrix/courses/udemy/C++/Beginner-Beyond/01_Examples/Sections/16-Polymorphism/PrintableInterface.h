f4435abb1a21296196985105ec8ff2d448fc1e42
LordSahvith <bulletproofchild@gmail.com>
2025-02-28 01:50:43 -0700

    add default virtual destructor to printable interface

--- matrix/courses/udemy/C++/Beginner-Beyond/01_Examples/Sections/16-Polymorphism/PrintableInterface.h
+++ matrix/courses/udemy/C++/Beginner-Beyond/01_Examples/Sections/16-Polymorphism/PrintableInterface.h
@@ -6,5 +6,6 @@ class I_Printable
     friend std::ostream& operator<<(std::ostream& os, const I_Printable& obj);
 
   public:
+    virtual ~I_Printable() = default;
     virtual void print(std::ostream& os) const = 0;
 };
