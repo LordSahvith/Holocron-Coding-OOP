f9a893c3eb3f789d5347b8edc07d6a2a03b7f163
LordSahvith <bulletproofchild@gmail.com>
2025-03-03 15:48:31 -0700

    add movement; create a struct

--- matrix/courses/udemy/C++/Make-Games-Raylib/01-Axe-Game/main.cpp
+++ matrix/courses/udemy/C++/Make-Games-Raylib/01-Axe-Game/main.cpp
@@ -1,51 +1,98 @@
 #include "raylib.h"
 
+struct Shape
+{
+    int xPos;
+    int yPos;
+    int width;
+    int height;
+    int moveSpeed;
+    Color color;
+
+    int GetRightEdge(int width) const
+    {
+        return xPos + width;
+    }
+    int GetLeftEdge(int width) const
+    {
+        return xPos - width;
+    }
+    int GetTopEdge(int height) const
+    {
+        return yPos - height;
+    }
+    int GetBottomEdge(int height) const
+    {
+        return yPos + height;
+    }
+};
+
+void PlayerMovement(Shape& shapeRef);
+
 int main()
 {
     // Window
-    int width{500};
-    int height{400};
-    InitWindow(width, height, "Axe Game");
+    int windowWidth{500};
+    int windowHeight{400};
+    InitWindow(windowWidth, windowHeight, "Axe Game");
 
     // Game Setup
     SetTargetFPS(60);
 
     // Circle Coordinates
-    int circleX{width / 2};
-    int circleY{height / 2};
+    int circleX{windowWidth / 2};
+    int circleY{windowHeight / 2};
+    int circleRadius{25};
+    Shape circle{circleX, circleY, circleRadius, circleRadius, 5, RED};
 
-    // Player Settings
-    int playerSpeed{5};
+    // Rectangle Coordinates
+    int rectX{windowWidth / 2};
+    int rectY{windowHeight / 2};
+    int rectWidth{50};
+    int rectHeight{50};
+    Shape rectangle{rectX, rectY, rectWidth, rectHeight, 10, RED};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawCircle(circleX, circleY, 25, RED);
+        DrawCircle(circle.xPos, circle.yPos, circle.width, RED);
+        DrawRectangle(rectangle.xPos, rectangle.yPos, rectangle.width, rectangle.height, BLUE);
 
-        if (IsKeyDown(KEY_D)) // move right
-        {
-            circleX += playerSpeed;
-        }
+        PlayerMovement(circle);
 
-        if (IsKeyDown(KEY_A)) // move left
+        rectangle.yPos += rectangle.moveSpeed;
+        if (rectangle.GetBottomEdge(rectangle.height) >= GetScreenHeight() || rectangle.GetTopEdge(0) <= 0)
         {
-            circleX -= playerSpeed;
-        }
-
-        if (IsKeyDown(KEY_W)) // move up
-        {
-            circleY -= playerSpeed;
-        }
-
-        if (IsKeyDown(KEY_S)) // move down
-        {
-            circleY += playerSpeed;
+            rectangle.moveSpeed = -rectangle.moveSpeed;
         }
 
         EndDrawing();
     }
 
     return 0;
+}
+
+void PlayerMovement(Shape& shapeRef)
+{
+    if (IsKeyDown(KEY_D) && shapeRef.GetRightEdge(shapeRef.width) < GetScreenWidth()) // move right
+    {
+        shapeRef.xPos += shapeRef.moveSpeed;
+    }
+
+    if (IsKeyDown(KEY_A) && shapeRef.GetLeftEdge(shapeRef.width) > 0) // move left
+    {
+        shapeRef.xPos -= shapeRef.moveSpeed;
+    }
+
+    if (IsKeyDown(KEY_W) && shapeRef.GetTopEdge(shapeRef.height) > 0) // move up
+    {
+        shapeRef.yPos -= shapeRef.moveSpeed;
+    }
+
+    if (IsKeyDown(KEY_S) && shapeRef.GetBottomEdge(shapeRef.height) < GetScreenHeight()) // move down
+    {
+        shapeRef.yPos += shapeRef.moveSpeed;
+    }
 }
