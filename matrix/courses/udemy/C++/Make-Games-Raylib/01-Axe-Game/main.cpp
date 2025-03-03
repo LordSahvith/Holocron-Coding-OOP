b327e232c005a63467206bc0e403d0cc315b13e6
LordSahvith <bulletproofchild@gmail.com>
2025-03-03 14:00:50 -0700

    Move a circle on screen

--- matrix/courses/udemy/C++/Make-Games-Raylib/01-Axe-Game/main.cpp
+++ matrix/courses/udemy/C++/Make-Games-Raylib/01-Axe-Game/main.cpp
@@ -2,19 +2,48 @@
 
 int main()
 {
+    // Window
     int width{500};
     int height{400};
     InitWindow(width, height, "Axe Game");
+
+    // Game Setup
+    SetTargetFPS(60);
+
+    // Circle Coordinates
+    int circleX{width / 2};
+    int circleY{height / 2};
+
+    // Player Settings
+    int playerSpeed{5};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
 
-        int xPos{width / 2};
-        int yPos{height / 2};
-        DrawCircle(xPos, yPos, 25, RED);
-        
+        DrawCircle(circleX, circleY, 25, RED);
+
+        if (IsKeyDown(KEY_D)) // move right
+        {
+            circleX += playerSpeed;
+        }
+
+        if (IsKeyDown(KEY_A)) // move left
+        {
+            circleX -= playerSpeed;
+        }
+
+        if (IsKeyDown(KEY_W)) // move up
+        {
+            circleY -= playerSpeed;
+        }
+
+        if (IsKeyDown(KEY_S)) // move down
+        {
+            circleY += playerSpeed;
+        }
+
         EndDrawing();
     }
 
