1a229b385bcafadfa3ad4a13502412102cac227b
LordSahvith <bulletproofchild@gmail.com>
2025-03-03 19:40:57 -0700

    add gravity and jump

--- matrix/courses/udemy/C++/Make-Games-Raylib/02-Dapper-Dasher/main.cpp
+++ matrix/courses/udemy/C++/Make-Games-Raylib/02-Dapper-Dasher/main.cpp
@@ -10,10 +10,40 @@ int main()
     // setup Frames Per Second (FPS)
     SetTargetFPS(60);
 
+    // acceleration due to gravity (pixels/frame) / frame
+    const int gravity{1};
+
+    // rectangle
+    const int width{50};
+    const int height{80};
+    int yPos{screenHeight - height};
+    int velocity{10};
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
+
+        // perform ground check
+        if (yPos >= screenHeight - height)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity += gravity;
+        }
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        // update position
+        yPos += velocity;
+
+        DrawRectangle(screenWidth / 2, yPos, width, height, RED);
+
         EndDrawing();
     }
 
