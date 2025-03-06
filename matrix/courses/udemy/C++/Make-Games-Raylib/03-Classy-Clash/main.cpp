940748866c51d08b5e1dddf70245d963dadc128b
LordSahvith <bulletproofchild@gmail.com>
2025-03-05 18:53:40 -0700

    create Map Class

--- matrix/courses/udemy/C++/Make-Games-Raylib/03-Classy-Clash/main.cpp
+++ matrix/courses/udemy/C++/Make-Games-Raylib/03-Classy-Clash/main.cpp
@@ -1,6 +1,44 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Map
+{
+  public:
+    Map(Texture2D map, Vector2 pos, float scale, float speed)
+        : MapImage{map}, Position{pos}, Scale{scale}, Speed{speed} {};
+    Texture2D GetMap() const { return MapImage; }
+    int GetMapWidth() const
+    {
+        return MapImage.width * Scale;
+    }
+    int GetMapHeight() const
+    {
+        return MapImage.height * Scale;
+    }
+    Vector2 GetPosition() const
+    {
+        return Position;
+    }
+    void SetPosition(Vector2 pos)
+    {
+        Position = pos;
+    }
+    float GetScale() const
+    {
+        return Scale;
+    }
+    float GetSpeed() const
+    {
+        return Speed;
+    }
+
+  private:
+    Texture2D MapImage;
+    Vector2 Position;
+    float Scale;
+    float Speed;
+};
+
 int main()
 {
     // setup window
@@ -10,16 +48,23 @@ int main()
 
     // Map Settings
     const Texture2D MAIN_MAP = LoadTexture("nature_tileset/WorldMap.png");
-    Vector2 mapPos{0.0, 0.0};
-    const float MAP_SCALE{4.0};
-    float cameraSpeed{4.0};
+    Vector2 mapPos{0.0f, 0.0f};
+    const float MAP_SCALE{4.0f};
+    float cameraSpeed{4.0f};
+    Map MainMap{MAIN_MAP, mapPos, MAP_SCALE, cameraSpeed};
 
     // Character Settings
+    Texture2D knightIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knightRun = LoadTexture("characters/knight_run_spritesheet.png");
     Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 knightPos {
-        (float)WINDOW_WIDTH / 2.0f - (MAP_SCALE * (0.5f * (float)knight.width / 6.0f)),
-        (float)WINDOW_HEIGHT / 2.0f - (MAP_SCALE * (0.5f * (float)knight.height))
-    };
+    Vector2 knightPos{(float)WINDOW_WIDTH / 2.0f - (MAP_SCALE * (0.5f * (float)knight.width / 6.0f)),
+                      (float)WINDOW_HEIGHT / 2.0f - (MAP_SCALE * (0.5f * (float)knight.height))};
+    float rightLeft{1.0f}; // 1: right, -1: left
+    // animation variables
+    float runningTime{0.0f};
+    int frame{0};
+    const int maxFrames{6};
+    const float updateTime{1.0f / 12.0f};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -28,27 +73,56 @@ int main()
         ClearBackground(BLACK);
 
         Vector2 direction{};
-        if (IsKeyDown(KEY_A) && mapPos.x < 0)
-            direction.x -= 1.0;
-        if (IsKeyDown(KEY_D) && mapPos.x > -(MAIN_MAP.width * MAP_SCALE - WINDOW_WIDTH))
-            direction.x += 1.0;
-        if (IsKeyDown(KEY_W) && mapPos.y < 0)
-            direction.y -= 1.0;
-        if (IsKeyDown(KEY_S) && mapPos.y > -(MAIN_MAP.height * MAP_SCALE - WINDOW_HEIGHT))
-            direction.y += 1.0;
-
-        if (Vector2Length(direction) != 0.0)
+        if (IsKeyDown(KEY_A) && MainMap.GetPosition().x < 0)
         {
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), cameraSpeed));
+            direction.x -= 1.0f;
+            rightLeft = -1.0f;
+        }
+        if (IsKeyDown(KEY_D) && MainMap.GetPosition().x > -(MainMap.GetMapWidth() - WINDOW_WIDTH))
+        {
+            direction.x += 1.0f;
+            rightLeft = 1.0f;
+        }
+        if (IsKeyDown(KEY_W) && MainMap.GetPosition().y < 0)
+        {
+            direction.y -= 1.0f;
+        }
+        if (IsKeyDown(KEY_S) && MainMap.GetPosition().y > -(MainMap.GetMapHeight() - WINDOW_HEIGHT))
+        {
+            direction.y += 1.0f;
+        }
+
+        if (Vector2Length(direction) != 0.0f)
+        {
+            Vector2 newPos = Vector2Subtract(MainMap.GetPosition(), Vector2Scale(Vector2Normalize(direction), cameraSpeed));
+            MainMap.SetPosition(newPos);
+            knight = knightRun;
+        }
+        else
+        {
+            knight = knightIdle;
         }
 
         // draw map
-        DrawTextureEx(MAIN_MAP, mapPos, 0, MAP_SCALE, WHITE);
+        DrawTextureEx(MainMap.GetMap(), MainMap.GetPosition(), 0, MAP_SCALE, WHITE);
+
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.0f;
+            if (frame > maxFrames)
+            {
+                frame = 0;
+            }
+        }
 
         // draw character
-        Rectangle source{0.f, 0.f, (float)knight.width / 6.f, (float)knight.height};
-        Rectangle destination{knightPos.x, knightPos.y, MAP_SCALE * (knight.width / 6.f), MAP_SCALE * (knight.height)};
-        DrawTexturePro(knight, source, destination, Vector2{}, 0.f, WHITE);
+        Rectangle source{frame * (float)knight.width / 6.0f, 0.0f, rightLeft * ((float)knight.width / 6.0f),
+                         (float)knight.height};
+        Rectangle destination{knightPos.x, knightPos.y, MAP_SCALE * (knight.width / 6.0f), MAP_SCALE * (knight.height)};
+        DrawTexturePro(knight, source, destination, Vector2{}, 0.0f, WHITE);
 
         EndDrawing();
     }
