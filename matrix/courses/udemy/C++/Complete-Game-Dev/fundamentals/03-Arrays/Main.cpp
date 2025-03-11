3d46df2a131426782d140da1db3f8178a19e6e3a
LordSahvith <bulletproofchild@gmail.com>
2025-03-11 15:23:29 -0600

    arrays

---
+++ matrix/courses/udemy/C++/Complete-Game-Dev/fundamentals/03-Arrays/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    // array of strings - [] denotes array
+    std::string employees[]{"steve", "bob", "allen"};
+
+    std::cout << employees[0] << std::endl; // steve
+    std::cout << employees[1] << std::endl; // bob
+    std::cout << employees[2] << std::endl; // allen
+
+    // throws: array out of bounds call
+    // std::cout << employees[3] << std::endl; // there is no 4th index
+
+    employees[2] = "savith";
+    std::cout << employees[2] << std::endl;
+
+    return 0;
+}
