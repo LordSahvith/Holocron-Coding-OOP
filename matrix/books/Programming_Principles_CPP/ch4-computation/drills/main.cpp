9cca8857f85eab9ef851eab810aa59e30b455e6b
LordSahvith <bulletproofchild@gmail.com>
2025-03-13 01:38:55 -0600

    drill: 4.9

--- matrix/books/Programming_Principles_CPP/ch4-computation/drills/main.cpp
+++ matrix/books/Programming_Principles_CPP/ch4-computation/drills/main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    constexpr double cm_per_in{2.54};
+    constexpr double in_per_ft{12.0};
     constexpr double cm_per_m{100.0};
 
     double sum{0};
@@ -9,15 +11,14 @@ int main()
     double largestNum{0};
 
     double currentNum{0};
+    string unit{' '};
     bool isFirstLoop{true};
 
-    cout << "Please enter a length followed by a unit (m): ";
+    cout << "Please enter a length followed by a unit (in, ft, cm, m): ";
 
-    while (cin >> currentNum)
+    while (cin >> currentNum >> unit)
     {
-        sum += currentNum;
-
-        cout << currentNum << "m == " << currentNum * cm_per_m << "cm" << endl;
+        double converted{0.0};
 
         if (isFirstLoop)
         {
@@ -25,14 +26,48 @@ int main()
             isFirstLoop = false;
         }
 
-        if (currentNum < smallestNum)
+        if (unit == "in")
         {
-            smallestNum = currentNum;
+            double cm{cm_per_in * currentNum};
+            converted = cm / cm_per_m;
+            sum += converted;
+
+            cout << currentNum << "in == " << cm << "cm" << endl;
+        }
+        else if (unit == "ft")
+        {
+            double in{in_per_ft * currentNum};
+            double cm{cm_per_in * in};
+            converted = cm / cm_per_m;
+            sum += converted;
+
+            cout << currentNum << "ft == " << in << "in" << endl;
+        }
+        else if (unit == "cm")
+        {
+            converted = currentNum / cm_per_m;
+            sum += converted;
+            cout << currentNum << "cm == " << currentNum / cm_per_in << "in" << endl;
+        }
+        else if (unit == "m")
+        {
+            converted = currentNum;
+            sum += converted;
+            cout << currentNum << "m == " << currentNum * cm_per_m << "cm" << endl;
+        }
+        else
+        {
+            cout << "Sorry, that unit is not available." << endl;
         }
 
-        if (currentNum > largestNum)
+        if (converted < smallestNum)
         {
-            largestNum = currentNum;
+            smallestNum = converted;
+        }
+
+        if (converted > largestNum)
+        {
+            largestNum = converted;
         }
     }
 
