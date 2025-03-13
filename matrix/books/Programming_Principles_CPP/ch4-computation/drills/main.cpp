9e4f4a517b26cb5bab259d77531c3cd03ef02218
LordSahvith <bulletproofchild@gmail.com>
2025-03-12 21:23:31 -0600

    drill: 4.5

--- matrix/books/Programming_Principles_CPP/ch4-computation/drills/main.cpp
+++ matrix/books/Programming_Principles_CPP/ch4-computation/drills/main.cpp
@@ -11,6 +11,11 @@ int main()
         {
             cout << "the smaller number is: " << num2 << endl;
             cout << "the larger number is: " << num1 << endl;
+
+            if (num1 - num2 <= 1.0 / 100)
+            {
+                cout << "the numbers are almost equal" << endl;
+            }
         }
         else if (num1 == num2)
         {
@@ -20,6 +25,11 @@ int main()
         {
             cout << "the smaller number is: " << num1 << endl;
             cout << "the larger number is: " << num2 << endl;
+
+            if (num2 - num1 <= 1.0 / 100)
+            {
+                cout << "the numbers are almost equal" << endl;
+            }
         }
     }
 }
