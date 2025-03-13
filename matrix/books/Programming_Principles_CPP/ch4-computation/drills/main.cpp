8c6c303501421042400baaafb035acdb2be6a04e
LordSahvith <bulletproofchild@gmail.com>
2025-03-12 21:13:33 -0600

    drill: 4.1

---
+++ matrix/books/Programming_Principles_CPP/ch4-computation/drills/main.cpp
@@ -0,0 +1,12 @@
+#include "../../00_std_lib_facilities.h"
+
+int main()
+{
+    int num1{0};
+    int num2{0};
+
+    while (cin >> num1 >> num2)
+    {
+        cout << num1 << ", " << num2 << endl;
+    }
+}
